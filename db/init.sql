0445ceb1d426662db5055728944ad5bcbfc7316f
ATHOOS <43479148+ATHOOS@users.noreply.github.com>
2019-03-12 23:25:34 +0100

    Delete init.sql

--- db/init.sql
+++
@@ -1,6 +0,0 @@
-CREATE user 'web'@172.16.1.5 identified by 'viveleweb';
-CREATE user 'admin'@'localhost' identified by 'viveladministration';
-grant all privileges on *.* TO 'web'@172.16.1.5;
-grant all privileges on *.* TO 'admin'@'localhost';
-
-flush privileges;
